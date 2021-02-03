6e5ef562f5f6e2e585fd852940946e41147387e1
Shirvani <38842555+shirvash@users.noreply.github.com>
2021-02-03 15:49:21 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,10 +4,10 @@
 #sudo pip3 install django==1.9.4
 
 
-sudo service mysql restart
+#sudo service mysql restart
 
 
-sudo mysql -uroot -e "CREATE DATABASE IF NOT EXISTS myproject CHARACTER SET utf8 COLLATE utf8_unicode_ci;"
+#sudo mysql -uroot -e "CREATE DATABASE IF NOT EXISTS myproject CHARACTER SET utf8 COLLATE utf8_unicode_ci;"
 
 
 sudo rm -f /etc/nginx/sites-enabled/test.conf
@@ -20,8 +20,8 @@ sudo rm /etc/gunicorn.d/ask.py
 sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/hello.py
 sudo ln -s /home/box/web/etc/ask.py /etc/gunicorn.d/ask.py
 
-cd /home/box/web
-python3 ask/manage.py makemigrations
-python3 ask/manage.py migrate
+#cd /home/box/web
+#python3 ask/manage.py makemigrations
+#python3 ask/manage.py migrate
 
 bash restartServers.sh
