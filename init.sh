078235bf7a1cea141f58f673965adccd27606d55
Shirvani <38842555+shirvash@users.noreply.github.com>
2021-02-03 11:40:38 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -15,13 +15,13 @@ sudo rm -f /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 
 
-if test -e /etc/gunicorn.d/hello.py; then sudo rm /etc/gunicorn.d/hello.py; fi
-if test -e /etc/gunicorn.d/ask.py; then sudo rm /etc/gunicorn.d/ask.py; fi
+sudo rm /etc/gunicorn.d/hello.py
+sudo rm /etc/gunicorn.d/ask.py
 sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/hello.py
 sudo ln -s /home/box/web/etc/ask.py /etc/gunicorn.d/ask.py
 
 cd /home/box/web
-python ask/manage.py makemigrations
-python ask/manage.py migrate
+python3 ask/manage.py makemigrations
+python3 ask/manage.py migrate
 
 bash restartServers.sh
