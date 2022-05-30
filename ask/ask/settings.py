c4675a2e6ab8fb58374c1fdf0859bdc36e4e421e
shirvani <shirvash96@gmail.com>
2022-05-30 16:46:10 +0300

    change db to mysql

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -74,22 +74,22 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',#sqlite3',
-#         'NAME': 'myproject',
-#         'USER': 'root',
-#         'PASSWORD': '',
-#         'HOST': 'localhost',
-#     },
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'askdb',
+        'USER': 'django@localhost',
+        'PASSWORD': 'pass',
+        'HOST': 'localhost',
+    },
+}
 
 
 # Password validation
--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,6 @@
 
 sudo /etc/init.d/mysql start
-mysql -uroot -e "CREATE DATABASE djbase;"
-mysql -uroot -e "CREATE USER 'django@localhost' IDENTIFIED BY 'pass123';"
-mysql -uroot -e "GRANT ALL ON djbase.* TO 'django@localhost';"
+mysql -uroot -e "CREATE DATABASE askdb;"
+mysql -uroot -e "CREATE USER 'django@localhost' IDENTIFIED BY 'pass';"
+mysql -uroot -e "GRANT ALL ON askdb.* TO 'django@localhost';"
 mysql -uroot -e "FLUSH PRIVILEGES;"
