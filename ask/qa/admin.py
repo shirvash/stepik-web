6cb4350f8fe554321fbf4403212dcd90e3d6b5be
shirvani <shirvash96@gmail.com>
2022-05-30 16:42:57 +0300

    add qa models

--- ask/qa/admin.py
+++ ask/qa/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Question, Answer
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    pass
+
+
--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,27 +1,27 @@
-# from django.db import models
-# from django.contrib.auth.models import User
-# from django.urls import reverse
-#
-#
-# class Question(models.Model):
-#     title = models.CharField(max_length=255)
-#     text = models.TextField()
-#     added_at = models.DateTimeField(auto_now_add=True)
-#     rating = models.IntegerField(default=0)
-#     author = models.ForeignKey(User)
-#     likes = models.ManyToManyField(User, related_name='likes_users')
-#
-#     def get_url(self):
-#         return reverse('question', kwargs={'id': self.pk})
-#
-#     def __unicode__(self):
-#         return self.title
-#
-#     def __str__(self):
-#         return self.title
-#
-# class Answer(models.Model):
-#     text = models.TextField()
-#     added_at = models.DateTimeField(auto_now_add=True)
-#     question = models.ForeignKey(Question)
-#     author = models.ForeignKey(User)
+from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(User, on_delete=models.deletion.CASCADE)
+    likes = models.ManyToManyField(User, related_name='likes_users')
+
+    def get_url(self):
+        return reverse('question', kwargs={'id': self.pk})
+
+    def __unicode__(self):
+        return self.title
+
+    def __str__(self):
+        return self.title
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now_add=True)
+    question = models.ForeignKey(Question, on_delete=models.deletion.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.deletion.CASCADE)
