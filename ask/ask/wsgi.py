32e3d8ac47d62f356a7b19d4c12acd6ff086b7e5
shirvani <shirvash96@gmail.com>
2022-05-30 16:12:04 +0300

    change django version to 2.0.7 with python 3.4.3

--- ask/ask/wsgi.py
+++ ask/ask/wsgi.py
@@ -4,7 +4,7 @@ WSGI config for ask project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
@@ -14,4 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ask.settings")
 
 application = get_wsgi_application()
-
--- init.sh
+++ init.sh
@@ -7,8 +7,8 @@ sudo rm -f /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 
 
-sudo rm /etc/gunicorn.d/hello.py
-sudo rm /etc/gunicorn.d/ask.py
+sudo rm /etc/gunicorn.d/hello
+sudo rm /etc/gunicorn.d/ask
 sudo ln -sf /home/box/web/etc/gunicorn-hello.conf /etc/gunicorn.d/hello
 sudo ln -sf /home/box/web/etc/gunicorn-ask.conf /etc/gunicorn.d/ask
 
