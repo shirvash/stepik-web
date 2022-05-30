93811a7c7d5e767d9f1d3b23fe99f7f1548d7a67
shirvani <shirvash96@gmail.com>
2022-05-30 15:24:35 +0300

    fix init.sh

--- init.sh
+++ init.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-#sudo rm -f /etc/nginx/sites-enabled/test.conf
-#sudo rm -f /etc/nginx/sites-enabled/default
-#sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
-#sudo rm /etc/gunicorn.d/hello.py
-#sudo rm /etc/gunicorn.d/ask.py
-#sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/hello.py
-#sudo ln -s /home/box/web/etc/ask.py /etc/gunicorn.d/ask.py
+sudo pip3 install --upgrade django
+sudo pip3 install --upgrade gunicorn
 
+sudo rm -f /etc/nginx/sites-enabled/test.conf
+sudo rm -f /etc/nginx/sites-enabled/default
+sudo rm /etc/gunicorn.d/test-django
+sudo rm /etc/gunicorn.d/test-wsgi
 sudo rm /etc/nginx/sites-enabled/default
+
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 
 sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
--- update-django.sh
+++
@@ -1,2 +0,0 @@
-sudo pip3 uninstall -y django
-sudo pip3 install -r requirements.txt
