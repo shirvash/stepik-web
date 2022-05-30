347d34f5da88df0f13b3698735ad1084edc37ae0
shirvani <shirvash96@gmail.com>
2022-05-30 18:33:41 +0300

    change db to mysql

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -75,22 +75,22 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'askdb',
-#         'USER': 'django@localhost',
-#         'PASSWORD': 'pass',
-#         'HOST': 'localhost',
-#     },
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'askdb',
+        'USER': 'django@localhost',
+        'PASSWORD': 'pass',
+        'HOST': 'localhost',
+    },
+}
 
 
 # Password validation
