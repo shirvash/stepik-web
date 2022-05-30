12c73766ee7e85047679f43294acd2466982d944
shirvani <shirvash96@gmail.com>
2022-05-30 16:53:50 +0300

    add QuestionManager

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -2,8 +2,15 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 
+class QuestionManager(models.Manager):
+    def new(self):
+        return self.order_by('-added_at')
+
+    def popular(self):
+        return self.order_by('-rating')
 
 class Question(models.Model):
+    objects = QuestionManager()
     title = models.CharField(max_length=255)
     text = models.TextField()
     added_at = models.DateTimeField(auto_now_add=True)
@@ -20,8 +27,12 @@ class Question(models.Model):
     def __str__(self):
         return self.title
 
+
 class Answer(models.Model):
     text = models.TextField()
     added_at = models.DateTimeField(auto_now_add=True)
     question = models.ForeignKey(Question, on_delete=models.deletion.CASCADE)
     author = models.ForeignKey(User, on_delete=models.deletion.CASCADE)
+
+
+
